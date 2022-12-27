d4245104e5ded9dc21449607003dfb1826cbaac3
Zironics <rafikobouloudene@gmail.com>
2022-12-28 00:48:02 +0100

    creation des models Prix,Prouit,Stock

--- ProjetSI2/gestionStock/admin.py
+++ ProjetSI2/gestionStock/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Prix,Stock,Produit
 
 # Register your models here.
+admin.site.register(Produit)
+admin.site.register(Stock)
+admin.site.register(Prix)
--- ProjetSI2/gestionStock/models.py
+++ ProjetSI2/gestionStock/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class Prix(models.Model):
+    PrixUnite = models.FloatField()
+    PrixVente = models.FloatField()
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['PrixUnite', 'PrixVente'], name='uniquePrix'
+            )
+        ]
+
+
+class Produit(models.Model):
+    CodeP = models.AutoField(primary_key=True)
+    designation = models.CharField(max_length=30)
+    prix = models.ManyToManyField(Prix, through='Stock',through_fields=('produit', 'PrixUnite','PrixVente'))
+    def __str__(self):
+        return (self.designation)
+
+class Stock(models.Model):
+    PrixUnite = models.ForeignKey(Prix,on_delete=models.CASCADE,related_name="StocksPrixU")
+    PrixVente = models.ForeignKey(Prix,on_delete=models.CASCADE,related_name="StocksPrixV")
+    produit = models.ForeignKey(Produit,on_delete=models.CASCADE,related_name="StockProduits")
+    Date = models.DateField(auto_now=True)
+    Qtp = models.IntegerField()
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['PrixUnite', 'PrixVente','produit'], name='uniqueStock'
+            )
+        ]
+    def __str__(self):
+        return (self.designation)
