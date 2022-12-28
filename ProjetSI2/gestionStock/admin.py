7f37387091a4809c1ba7f0228e3374dc912dcf51
Zironics <rafikobouloudene@gmail.com>
2022-12-28 18:21:16 +0100

    la suite des models

--- ProjetSI2/gestionStock/admin.py
+++ ProjetSI2/gestionStock/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Prix,Stock,Produit
-
+from .models import Prix,Stock,Produit,Composer,Vente,Client,ReglementVente
+from .models import Fournisseur,Facture,TypeProduit,ReglementFacture,Avoir,EntreeStock,SortieStock
 # Register your models here.
 admin.site.register(Produit)
 admin.site.register(Stock)
 admin.site.register(Prix)
+admin.site.register(Composer)
+admin.site.register(Vente)
+admin.site.register(Client)
+admin.site.register(ReglementVente)
+admin.site.register(ReglementFacture)
+admin.site.register(Fournisseur)
+admin.site.register(Facture)
+admin.site.register(TypeProduit)
+admin.site.register(Avoir)
+admin.site.register(EntreeStock)
+admin.site.register(SortieStock)
--- ProjetSI2/gestionStock/models.py
+++ ProjetSI2/gestionStock/models.py
@@ -12,25 +12,101 @@ class Prix(models.Model):
             )
         ]
 
+class TypeProduit(models.Model):
+    designation = models.CharField(max_length=20)
+    def __str__(self):
+        return (self.designation)
 
 class Produit(models.Model):
     CodeP = models.AutoField(primary_key=True)
     designation = models.CharField(max_length=30)
-    prix = models.ManyToManyField(Prix, through='Stock',through_fields=('produit', 'PrixUnite','PrixVente'))
+    prix = models.ManyToManyField(Prix, through='Stock',through_fields=('produit','Prix'))
+    typeP = models.ForeignKey(TypeProduit,on_delete=models.SET_NULL,null=True)
     def __str__(self):
         return (self.designation)
 
 class Stock(models.Model):
-    PrixUnite = models.ForeignKey(Prix,on_delete=models.CASCADE,related_name="StocksPrixU")
-    PrixVente = models.ForeignKey(Prix,on_delete=models.CASCADE,related_name="StocksPrixV")
+    Prix = models.ForeignKey(Prix,on_delete=models.CASCADE,related_name="StocksPrix")
     produit = models.ForeignKey(Produit,on_delete=models.CASCADE,related_name="StockProduits")
     Date = models.DateField(auto_now=True)
     Qtp = models.IntegerField()
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=['PrixUnite', 'PrixVente','produit'], name='uniqueStock'
+                fields=['Prix','produit'], name='uniqueStock'
             )
         ]
+
+class EntreeStock(models.Model):
+    date = models.DateField(auto_now=True)
+    qt = models.IntegerField()
+    produit = models.ForeignKey(Produit,on_delete=models.SET_NULL,null=True)
+
+class SortieStock(models.Model):
+    date = models.DateField(auto_now=True)
+    motif = models.CharField(max_length=30)
+    produit = models.ForeignKey(Produit,on_delete=models.SET_NULL,null=True)
+
+class Client(models.Model):
+    nom = models.CharField(max_length=50)
+    prenom = models.CharField(max_length=50)
+    adresse = models.CharField(max_length=50,blank = True)
+    phone = models.CharField(max_length=10,blank=True,null=True)
+    credit = models.FloatField(default=0,blank=True)
     def __str__(self):
-        return (self.designation)
+        return f'{self.nom} {self.prenom}'
+
+class Vente(models.Model):
+    Date = models.DateField(auto_now=True)
+    client = models.ForeignKey(Client,null=True,on_delete=models.SET_NULL)
+    restant = models.FloatField()
+    def __str__(self):
+        return f'{self.id} {self.Date}'
+   
+
+class Composer(models.Model):
+    produit = models.ForeignKey(Produit,on_delete=models.CASCADE)
+    prix = models.ForeignKey(Prix,on_delete=models.CASCADE)
+    vente = models.ForeignKey(Vente,on_delete=models.CASCADE)
+    QtV = models.IntegerField()
+    def __str__(self):
+        return f'{self.produit.designation} {self.prix.PrixVente} {self.QtV}'
+
+class ReglementVente(models.Model):
+    date = models.DateField(auto_now=True)
+    sommeAjoute = models.FloatField()
+    vente = models.ForeignKey(Vente,on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.vente} -  {self.sommeAjoute} -  {self.date}'
+
+class Fournisseur(models.Model):
+    nom = models.CharField(max_length=50)
+    prenom = models.CharField(max_length=50)
+    adresse = models.CharField(max_length=50,blank = True)
+    phone = models.CharField(max_length=10,blank=True,null=True)
+    solde = models.FloatField(default=0,blank=True)
+    def __str__(self):
+        return f'{self.nom} {self.prenom}'
+
+
+class Facture(models.Model):
+    date = models.DateField(auto_now=True)
+    sommeRestante = models.FloatField()
+    fournisseur = models.ForeignKey(Fournisseur,on_delete=models.SET_NULL,null=True)
+    def __str__(self):
+        return f'{self.date} {self.fournisseur.nom}'
+
+class Avoir(models.Model):
+    qta = models.IntegerField()
+    facture = models.ForeignKey(Facture,on_delete=models.CASCADE)
+    produit = models.ForeignKey(Produit,on_delete=models.CASCADE)
+    prix = models.ForeignKey(Prix,on_delete=models.CASCADE)
+
+class ReglementFacture(models.Model):
+    date = models.DateField(auto_now=True)
+    sommeAjoute = models.FloatField()
+    facture = models.ForeignKey(Facture,on_delete=models.CASCADE)
+
+
+
+
